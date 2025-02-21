ae3c4fc463d813d49086c7722175cbd14303715a
Mefodei <mefodei.link@gmail.com>
2025-02-22 01:18:32 +0700

    - update ecs async methods

---
+++ States/Systems/StateSystem.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Ecs.State.Systems
+{
+    using System;
+    using Leopotam.EcsProto;
+
+    [Serializable]
+    public abstract class StateSystem : IProtoSystem
+    {
+        public virtual string Name => GetType().Name;
+    }
+}
