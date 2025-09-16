7d1fe0a1c114648e4c4aae8d771c3286f8cfd12b
Mefodei <mefodei.link@gmail.com>
2025-09-16 22:08:26 +0700

    - update damage effects

--- GameEffects/DamageEffect/DamageEffectConfiguration.cs
+++ GameEffects/DamageEffect/DamageEffectConfiguration.cs
@@ -8,6 +8,7 @@
     using Leopotam.EcsProto;
     using UniGame.LeoEcs.Shared.Extensions;
     using UnityEngine;
+    using UnityEngine.Serialization;
 
     [Serializable]
     public sealed class DamageEffectConfiguration : EffectConfiguration
@@ -19,15 +20,15 @@
 
         #endregion
         
-        [SerializeField]
+        [FormerlySerializedAs("_damageValue")]
         [Min(0.0f)]
-        private float _damageValue;
+        public float damageValue;
         
         protected override void Compose(ProtoWorld world, ProtoEntity effectEntity)
         {
             var damagePool = world.GetPool<DamageEffectComponent>();
             ref var damage = ref damagePool.Add(effectEntity);
-            damage.Value = _damageValue;
+            damage.Value = damageValue;
             DamageType.Compose(world, effectEntity);
         }
     }
